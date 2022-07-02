3a2b17dd5c2c64a7c26ffef27e386756aa01f4a4
ThomasMlh <storm62320@gmail.com>
2022-07-02 12:15:57 +0200

    remove image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,11 @@
 import type { NextPage } from "next";
-import { Center, Image, Flex, Text } from "@chakra-ui/react";
+import { Center, Flex, Text } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   return (
     <Center h={"100vh"}>
       <Flex direction={"column"}>
         <Text align={"center"}>Hello</Text>
-        <Image
-          borderRadius={"full"}
-          boxSize="400px"
-          src={"https://avatars.githubusercontent.com/u/33006260?v=4"}
-        />
       </Flex>
     </Center>
   );
